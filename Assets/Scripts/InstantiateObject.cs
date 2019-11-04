7be2cd6b6a253429d71633e8ca78b8b9b815a7e5
Riku <riku.juhola@gmail.com>
2019-11-04 21:08:20 +0200

    Added items. SpawnLocs. RandomSpawn

---
+++ Assets/Scripts/InstantiateObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateObject : MonoBehaviour
+{
+
+        public GameObject myPrefab;
+        public Vector2 SpawnLoc;
+
+        void Start()
+        {
+
+         
+
+        }
+    }
---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+
+
+ //  private void OnDrawGizmos2D()
+ //  {
+ //      if  (collectableSpawnPoints.Length == 0)
+ //          return;
+ //
+ //      foreach (Transform Loc in collectableSpawnPoints)
+ //      {
+ //          if (Loc == null)
+ //          {
+ //              Gizmos.color = Color.red;
+ //              Gizmos.DrawSphere(Loc.position, 1f);
+ //          }
+ //      }
+ //  }
+
+    public static GameObject[] gameItems;
+    public int numToSpawn;
+    public static int numSpawned = 0;
+    public Transform SpawnPosition;
+    public Transform[] collectableSpawnPoints = new Transform[5];
+
+    void Start()
+    {
+        gameItems = Resources.LoadAll<GameObject>("SpawnItems");
+    }
+
+    void SpawnRandomObject()
+    {
+
+        // Get SpawnPoint
+        Transform spawnPoint = GetCollectableSpawnPoint();
+        int whichItem = Random.Range(0, 100);
+
+        GameObject sItem = Instantiate(gameItems[whichItem]) as GameObject;
+
+        numSpawned++;
+
+        sItem.transform.position = spawnPoint.position;
+    }
+
+    void Update()
+    {
+        if (numToSpawn > numSpawned)
+        {
+            
+            //where your instantiated object spawns from
+            SpawnRandomObject();
+        }
+    }
+
+    public Transform GetCollectableSpawnPoint()
+    {
+        //randomly selects a point out of the array
+
+        int index = Random.Range(0, collectableSpawnPoints.Length);
+
+        //returns the selected point
+        return collectableSpawnPoints[index];
+    }
+}
