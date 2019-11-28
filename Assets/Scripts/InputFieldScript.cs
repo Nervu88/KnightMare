88536dcd538d0b75003d71d5e541f03b49793c9c
Riku <riku.juhola@gmail.com>
2019-11-28 17:56:41 +0200

    Revert "upd"
    This reverts commit 1d8a8afdf8825efec6bada7fd5ebc85d2316aef3.

--- Assets/Scripts/InputFieldScript.cs
+++ Assets/Scripts/InputFieldScript.cs
@@ -5,25 +5,20 @@ using UnityEngine.UI;
 
 public class InputFieldScript : MonoBehaviour
 {
-    RiddleScript rld = new RiddleScript();
-    RandomRiddle door = new RandomRiddle();
     LevelManager mgr = new LevelManager();
     public InputField mainInputField;
     public int playerAnswer;
-    Animator anim = new Animator();
 
+    public void Start()
+    {
 
-    public void SubmitAnswer()
+    }
+
+    public int ReturnAnswer()
     {
         playerAnswer = int.Parse(mainInputField.text);
-        if (door.riddleAnswer == playerAnswer)
-        {
-            anim = door.GetComponent<Animator>();
-            anim.SetTrigger("OpenDoor");
-            door.gameObject.tag = "OpenDoor";
-            Debug.Log("Vastasit Oikein");
-            rld.inputBox.SetActive(false);
-            mgr.setContinue();
-        }
+        mgr.setContinue();
+        return playerAnswer;
     }
+
 }
